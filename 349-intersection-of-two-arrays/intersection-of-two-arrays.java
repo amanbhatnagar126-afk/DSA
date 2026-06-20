29314953b0fd14b06e10a785f77bebcdd2819212
Aman Bhatnagar <amanbhatnagar126@gmail.com>
2026-06-21 01:29:21 +0530

    Time: 2 ms (98.23%) | Memory: 45.3 MB (13.21%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.java
@@ -1,33 +1,24 @@
+import java.util.HashSet;
+import java.util.Set;
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-    
-        Arrays.sort(nums1);
-        Arrays.sort(nums2);
-
-        List<Integer> resultList = new ArrayList<>();
-
-        int i = 0;
-        int j = 0;
-
-        while (i < nums1.length && j < nums2.length){
-            if (nums1[i] < nums2[j]){
-                i++;
-            }
-            else if (nums1[i] > nums2[j]){
-                j++;
-            }
-            else{
-                if(resultList.isEmpty() || resultList.get(resultList.size() - 1) != nums1[i]){
-                    resultList.add(nums1[i]);
-                }
-                i++;
-                j++;
+        Set<Integer> set1 = new HashSet<>();
+        for (int num : nums1){
+            set1.add(num);
+        }
+        Set<Integer> intersectionSet = new HashSet<>();
+        for (int num : nums2){
+            if (set1.contains(num)){
+                intersectionSet.add(num);
             }
         }
-        int[]result = new int[resultList.size()];
-        for(int k = 0; k < resultList.size(); k++){
-            result[k] = resultList.get(k);
+        int[] result = new int[intersectionSet.size()];
+        int index = 0;
+        for (int num : intersectionSet){
+            result[index++] = num;
         }
         return result;
+
+        
     }
 }
