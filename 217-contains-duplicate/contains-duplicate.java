f117807c78a0ab0d3bdf17d34da6eae380ad9807
Aman Bhatnagar <amanbhatnagar126@gmail.com>
2026-07-10 19:38:17 +0530

    Time: 17 ms (48.29%) | Memory: 108.4 MB (5.83%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -3,9 +3,10 @@ class Solution {
         HashSet<Integer> SEEN = new HashSet<>();
 
         for(int num : nums){
-            if(!SEEN.add(num)){
+            if(SEEN.contains(num)){
                 return true;
             }
+            SEEN.add(num);
         }
         return false;
         
