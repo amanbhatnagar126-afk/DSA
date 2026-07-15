9e940fff12ba8e2a25d7d46e71b7751600e43fb3
Aman Bhatnagar <amanbhatnagar126@gmail.com>
2026-07-16 01:36:25 +0530

    Time: 0 ms (100.00%) | Memory: 45.4 MB (8.28%) - LeetSync

--- 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.java
+++ 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.java
@@ -8,14 +8,10 @@
  */
 class Solution {
     public void deleteNode(ListNode node) {
-        ListNode curr = node;
-        ListNode prev = null;
-
-        while(curr.next != null){
-            curr.val = curr.next.val;       //TC = O(N)
-            prev = curr;                    //SC = O(1)
-            curr = curr.next;
-        }
-        prev.next = null;
+        node.val = node.next.val;
+                                
+        node.next = node.next.next;
     }
 }
+//TC = O(1) performed exactly two constant-time oper., No loops/No traversals.
+//SC = O(1) X allocated any new variables or use extra space.
